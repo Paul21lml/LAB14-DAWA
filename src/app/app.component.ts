f49b462d30440acc94f9977e257335ea1db91352
Paul21lml <paul.ponce@tecsup.edu.pe>
2020-07-08 03:39:45 -0500

    1er commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  template:`<p>The hero's birthday is {{ birthday | date }}</p>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular14';
+  birthday = new Date(1988, 3, 15); 
+  heroes: any[] = [];
+  heroes2: any[] = [];
+  canFly = true;
+  mutate = true;
+  title= 'Heroes que vuelan';
+
+  constructor() { this.reset();}
+
+  addHero(name:string,birth:Date){
+    name=name.trim();
+    if(!name){ return;}
+    let hero = {name,birth,canFly:this.canFly};
+    this.heroes=this.heroes.concat(hero);
+  }
+
+
+  reset(){ this.heroes=[];}
 }
